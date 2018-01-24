91793cde72735d62f8ec41f98c8ad6455bcfdfe2
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-24 18:33:46 +0100

    primeros obstaculos

--- js/game.js
+++ js/game.js
@@ -3,16 +3,28 @@ var ctx = canvas.getContext('2d');
 
 
 var player = new Player(ctx, canvas);
-var lava = new Lava(ctx, canvas);
-//var obstacule = new Obstacule(ctx, canvas);
+// var obstacleOne = new Obstacles(250,450, 90, 20);
+// var obstacleTwo = new Obstacles(370,420, 90, 20);
+// var obstacleThree = new Obstacles(520,380, 90, 20);
+// var obstacleFour = new Obstacles(330,320, 90, 20);
+var obstacleBaseOne = new Obstacles (0, 460, 300, 40, '#000000');
+var obstacleBaseTwo = new Obstacles (350, 460, 300, 40);
+var obstacleBaseThree = new Obstacles (780, 460, 300, 40);
 
 function update() {
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
   player.render();
-  //lava.renderMap();
+  // obstacleOne.draw();
+  // obstacleTwo.draw();
+  // obstacleThree.draw();
+  // obstacleFour.draw();
+  obstacleBaseOne.draw();
+  obstacleBaseTwo.draw();
+  obstacleBaseThree.draw();
+ 
 };
-lava.renderMap();
+
 setInterval(update, 10)
 
 
